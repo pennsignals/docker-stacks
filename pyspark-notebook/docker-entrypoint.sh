e37c7a53b03f91baab25be07e8c30ddedc346eb8
rightlag <rightlag@gmail.com>
2017-03-06 15:00:49 -0500

    Add newlines to docker-entrypoint.sh

--- pyspark-notebook/docker-entrypoint.sh
+++ pyspark-notebook/docker-entrypoint.sh
@@ -1,1 +1,5 @@
-PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=9999" $SPARK_HOME/bin/pyspark --packages com.databricks:spark-avro_2.10:3.0.1 --master local[8]
+PYSPARK_DRIVER_PYTHON=jupyter \
+PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=9999" \
+$SPARK_HOME/bin/pyspark \
+--packages com.databricks:spark-avro_2.10:3.0.1 \
+--master local[8]
