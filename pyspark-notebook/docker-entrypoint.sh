5c60dcc47eb3a8939642c90a18258ffa38b0f085
rightlag <rightlag@gmail.com>
2017-03-06 23:15:08 -0500

    Update spark-avro to v2.11:3.2.0

--- pyspark-notebook/docker-entrypoint.sh
+++ pyspark-notebook/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 PYSPARK_DRIVER_PYTHON=jupyter \
 PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=9999" \
 $SPARK_HOME/bin/pyspark \
---packages com.databricks:spark-avro_2.10:3.0.1 \
+--packages com.databricks:spark-avro_2.11:3.2.0 \
 --master local[8]
